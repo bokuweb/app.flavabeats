2124d37f92eb327a258df466d9068600d3f35210
bokuweb <info@typebeats.com>
2015-03-06 09:06:51 +0900

    refact

--- js/main.coffee
+++ js/main.coffee
@@ -8,14 +8,6 @@ class @Game
     good : 0.2
 
   _game = null
-  _note =
-    index : 0
-    group : null
-    timing : []
-    key : []
-    speed : 0
-
-  _music = null
   _status = "stop"
   _pool =
     note : []
@@ -25,24 +17,22 @@ class @Game
     actual:0
     shown:0
 
+  _n = null
+
   constructor : (parms)->
     enchant()
     _game = new Core(980, 600)
     _game.fps = 60
     _game.preload("img/chara1.png", "img/chara1_shadow.png", "img/logo.png", "img/score.png")
-    _note.group = new Group()
-    _note.index = 0
 
   play : (music)->
     _game.preload(music.src, music.img)
     _endTime = music.endTime
-    _note.timing = music.note.timing
-    _note.key = music.note.key
-    _note.speed = music.note.speed
+    _n = new Note(_game, music.note)
     _game.start()
 
     _game.onload = ->
-      _music = _game.assets[music.src]
+      _game.music = _game.assets[music.src]
       _score.board = new Score(
         game : _game
         offsetX : 20
@@ -52,51 +42,21 @@ class @Game
         width: 36
         height: 49.7
         )
+      _n.init()
       _score.board.generate()
       _score.board.update(0)
-      _game.rootScene.addChild(_note.group)
-      _renderTarget()
-      _preAllocateNote()
-
-      #
-      # main animation frame
-      # check should notes be generated
-      # check game end timng
-      #
       _game.rootScene.addEventListener "enterframe", ->
-        music = _music
-        note  = _note
         if _status is "playing"
-          if note.timing[note.index]?
-            if music.currentTime > (note.timing[note.index] - (_fallDist / note.speed)) - (1 / _game.fps)
-              _generateNote(note.index++)
-
+           _n.create()
           # score update
           if _score.actual > _score.shown
             _score.shown += 100000 / _note.timing.length / 6
             if _score.actual < _score.shown then _score.shown = _score.actual
             _score.board.update(Math.ceil(_score.shown))
-
         _endGameIfTimeOver()
 
-  _renderTarget = -> 
-    for i in [0...5]
-      noteShadow = new Sprite(_noteWidth, _noteHeight)
-      noteShadow.y = -_noteHeight + _fallDist  
-      noteShadow.opacity = 0.6
-      noteShadow.x = i * ( _noteWidth + 20) + 480
-      noteShadow.image = _game.assets["img/chara1_shadow.png"]
-      _game.rootScene.addChild(noteShadow)
-    return
-
-  _preAllocateNote = -> 
-    for v, i in _note.timing
-      note = new Sprite(_noteWidth, _noteHeight)
-      note.image = _game.assets["img/chara1.png"]
-      _poolSprite(_pool.note, note)
-    return
-
-  _endGameIfTimeOver = -> 
+  _endGameIfTimeOver = ->
+    music = _game.music
     if music.currentTime >= _endTime or music.duration <= music.currentTime
       if music.volume - 0.1 < 0 then music.volume = 0 else music.volume -= 0.1
       if music.volume <= 0
@@ -106,86 +66,9 @@ class @Game
         _status = "stop"
         #_game.callback.end()
 
-  _generateNote = (number)->
-    note = _getSprite(_pool.note)
-    note.number = number
-    note.key = _note.key[number]
-    note.destinationY = -_noteHeight + _fallDist  
-    note.y = -_noteHeight
-    note.x = note.key * (note.width+20) + 480
-    note.frame  = 0
-    note.timing = _note.timing[number]
-    note.clear  = false
-    note.opacity = 1
-    note.tl.clear()
-    note.tl.setTimeBased()
-    note.tl.scaleTo(1, 1, 0)
-    note.tl.moveY(note.destinationY, (_fallDist / _note.speed) * 1000)
-    note.hasClearAnimationStarted = false
-    _note.group.addChild(note)
-
-    #
-    # note animation frame
-    # monitor whether there is outdated notes
-    #
-    note.addEventListener "enterframe", ->
-      music = _music
-      note  = _note
-      if @oldtime?
-        @rotate((music.currentTime - @oldtime) * 500)
-      @oldtime = music.currentTime
-
-      if note.timing[@number] - music.currentTime < -1
-        @tl.fadeOut(300).then(()->
-          note.group.removeChild(@)
-        )
-      #
-      # clear note animation
-      # define animation when note cleared
-      #
-      if @clear and not @hasClearAnimationStarted
-        @tl.clear()
-        @tl.scaleTo(1.5, 1.5, 200).and().fadeOut(200).then(()->
-          note.group.removeChild(@)
-        )
-        if -_threshold.great < (note.timing[@number] - @clearTime) < _threshold.great
-          judge = "great"
-          _game.score.actual += 100000 / _note.timing.length
-        else if -_threshold.good < (note.timing[@number] - @clearTime) < _threshold.good
-          judge = "good"
-          _game.score.actual += 70000 / _note.timing.length
-        else
-          judge = "bad"
-
-        judgeLabel = new Label(judge)
-        judgeLabel.x = 450
-        judgeLabel.y = 450
-        _game.rootScene.addChild(judgeLabel)
-
-        judgeLabel.tl.setTimeBased()
-        judgeLabel.tl.fadeOut(300).and().moveY(400, 300).then(()->
-          _game.rootScene.removeChild(judgeLabel)
-        )
-        @hasClearAnimationStarted = true
-
-  _poolSprite = (pool, sprite)->
-    pool.push(sprite)
-    sprite.active = false
-    if sprite.addEventListener
-      sprite.addEventListener "removed", ()->
-        @active = false
-
-  _getSprite = (pool)->
-    for value, i in pool
-      unless value.active
-        value.active = true
-        return value
-    console.log "error sprite pool empty"
-    return false
-
   # keydown event detected
   document.addEventListener "keydown", (e)->
-    music = _music
+    music = _game.music
     if _status is "stop"
       if e.keyCode is 13
         music.play()
@@ -210,6 +93,7 @@ class @Game
 
 class Note
   _game = null
+  _pool = []
   _fallDist = 550
   _noteWidth = 64
   _noteHeight = 64
@@ -220,10 +104,10 @@ class Note
     key : []
     speed : 0
 
-  constructor : (game, parms)->
+  constructor : (game, params)->
     _game = game
-    _pool = []
     _note.timing = params.timing
+    console.log _note.timing
     _note.key = params.key
     _note.speed = params.speed
 
@@ -231,30 +115,30 @@ class Note
     _note.group = new Group()
     _note.index = 0
     _renderDist()
-    _preAllocateNote()
+    _preAllocate()
     _game.rootScene.addChild(_note.group)
-    
-  create : (currentTime)->
-    if _note.timing[_note.index]?
-      if currentTime > (_note.timing[_note.index] - (_fallDist / _note.speed)) then _gen(note.index++)
 
-  _renderDist = -> 
+  create : ->
+    if _note.timing[_note.index]?
+      if _game.music.currentTime > (_note.timing[_note.index] - (_fallDist / _note.speed)) then _gen(_note.index++)
+
+  _renderDist = ->
     for i in [0...5]
       noteShadow = new Sprite(_noteWidth, _noteHeight)
-      noteShadow.y = -_noteHeight + _fallDist  
+      noteShadow.y = -_noteHeight + _fallDist
       noteShadow.opacity = 0.6
       noteShadow.x = i * ( _noteWidth + 20) + 480
       noteShadow.image = _game.assets["img/chara1_shadow.png"]
       _game.rootScene.addChild(noteShadow)
     return
 
-  _preAllocate = -> 
+  _preAllocate = ->
     for v in _note.timing
       note = new Sprite(_noteWidth, _noteHeight)
-      note.image = GameSys.getResouce("img/chara1.png")
+      note.image = _game.assets["img/chara1.png"]
       GameSys.poolSprite(_pool, note)
     return
-    
+
   _gen = (number)->
     note = GameSys.getSprite(_pool)
     note.number = number
@@ -277,7 +161,7 @@ class Note
     # monitor whether there is outdated notes
     #
     note.addEventListener "enterframe", ->
-      music = _music
+      music = _game.music
       note  = _note
       if @oldtime?
         @rotate((music.currentTime - @oldtime) * 500)
@@ -333,4 +217,4 @@ class GameSys
       sprite.addEventListener "removed", ()->
         @active = false
 
-  @getResouce : (src)-> _game.assets[src]
+  #@getResouce : (src)-> _game.assets[src]
--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
   var GameSys, Note;
 
   this.Game = (function() {
-    var _endGameIfTimeOver, _endTime, _fallDist, _game, _generateNote, _getSprite, _music, _note, _noteHeight, _noteWidth, _pool, _poolSprite, _preAllocateNote, _renderTarget, _score, _status, _threshold;
+    var _endGameIfTimeOver, _endTime, _fallDist, _game, _n, _noteHeight, _noteWidth, _pool, _score, _status, _threshold;
 
     _fallDist = 550;
 
@@ -20,16 +20,6 @@
 
     _game = null;
 
-    _note = {
-      index: 0,
-      group: null,
-      timing: [],
-      key: [],
-      speed: 0
-    };
-
-    _music = null;
-
     _status = "stop";
 
     _pool = {
@@ -42,24 +32,22 @@
       shown: 0
     };
 
+    _n = null;
+
     function Game(parms) {
       enchant();
       _game = new Core(980, 600);
       _game.fps = 60;
       _game.preload("img/chara1.png", "img/chara1_shadow.png", "img/logo.png", "img/score.png");
-      _note.group = new Group();
-      _note.index = 0;
     }
 
     Game.prototype.play = function(music) {
       _game.preload(music.src, music.img);
       _endTime = music.endTime;
-      _note.timing = music.note.timing;
-      _note.key = music.note.key;
-      _note.speed = music.note.speed;
+      _n = new Note(_game, music.note);
       _game.start();
       return _game.onload = function() {
-        _music = _game.assets[music.src];
+        _game.music = _game.assets[music.src];
         _score.board = new Score({
           game: _game,
           offsetX: 20,
@@ -69,58 +57,28 @@
           width: 36,
           height: 49.7
         });
+        _n.init();
         _score.board.generate();
         _score.board.update(0);
-        _game.rootScene.addChild(_note.group);
-        _renderTarget();
-        _preAllocateNote();
         return _game.rootScene.addEventListener("enterframe", function() {
-          var note;
-          music = _music;
-          note = _note;
           if (_status === "playing") {
-            if (note.timing[note.index] != null) {
-              if (music.currentTime > (note.timing[note.index] - (_fallDist / note.speed)) - (1 / _game.fps)) {
-                _generateNote(note.index++);
-              }
+            _n.create();
+          }
+          if (_score.actual > _score.shown) {
+            _score.shown += 100000 / _note.timing.length / 6;
+            if (_score.actual < _score.shown) {
+              _score.shown = _score.actual;
             }
-            if (_score.actual > _score.shown) {
-              _score.shown += 100000 / _note.timing.length / 6;
-              if (_score.actual < _score.shown) {
-                _score.shown = _score.actual;
-              }
-              _score.board.update(Math.ceil(_score.shown));
-            }
+            _score.board.update(Math.ceil(_score.shown));
           }
           return _endGameIfTimeOver();
         });
       };
     };
 
-    _renderTarget = function() {
-      var i, noteShadow, _i;
-      for (i = _i = 0; _i < 5; i = ++_i) {
-        noteShadow = new Sprite(_noteWidth, _noteHeight);
-        noteShadow.y = -_noteHeight + _fallDist;
-        noteShadow.opacity = 0.6;
-        noteShadow.x = i * (_noteWidth + 20) + 480;
-        noteShadow.image = _game.assets["img/chara1_shadow.png"];
-        _game.rootScene.addChild(noteShadow);
-      }
-    };
-
-    _preAllocateNote = function() {
-      var i, note, v, _i, _len, _ref;
-      _ref = _note.timing;
-      for (i = _i = 0, _len = _ref.length; _i < _len; i = ++_i) {
-        v = _ref[i];
-        note = new Sprite(_noteWidth, _noteHeight);
-        note.image = _game.assets["img/chara1.png"];
-        _poolSprite(_pool.note, note);
-      }
-    };
-
     _endGameIfTimeOver = function() {
+      var music;
+      music = _game.music;
       if (music.currentTime >= _endTime || music.duration <= music.currentTime) {
         if (music.volume - 0.1 < 0) {
           music.volume = 0;
@@ -136,90 +94,9 @@
       }
     };
 
-    _generateNote = function(number) {
-      var note;
-      note = _getSprite(_pool.note);
-      note.number = number;
-      note.key = _note.key[number];
-      note.destinationY = -_noteHeight + _fallDist;
-      note.y = -_noteHeight;
-      note.x = note.key * (note.width + 20) + 480;
-      note.frame = 0;
-      note.timing = _note.timing[number];
-      note.clear = false;
-      note.opacity = 1;
-      note.tl.clear();
-      note.tl.setTimeBased();
-      note.tl.scaleTo(1, 1, 0);
-      note.tl.moveY(note.destinationY, (_fallDist / _note.speed) * 1000);
-      note.hasClearAnimationStarted = false;
-      _note.group.addChild(note);
-      return note.addEventListener("enterframe", function() {
-        var judge, judgeLabel, music, _ref, _ref1;
-        music = _music;
-        note = _note;
-        if (this.oldtime != null) {
-          this.rotate((music.currentTime - this.oldtime) * 500);
-        }
-        this.oldtime = music.currentTime;
-        if (note.timing[this.number] - music.currentTime < -1) {
-          this.tl.fadeOut(300).then(function() {
-            return note.group.removeChild(this);
-          });
-        }
-        if (this.clear && !this.hasClearAnimationStarted) {
-          this.tl.clear();
-          this.tl.scaleTo(1.5, 1.5, 200).and().fadeOut(200).then(function() {
-            return note.group.removeChild(this);
-          });
-          if ((-_threshold.great < (_ref = note.timing[this.number] - this.clearTime) && _ref < _threshold.great)) {
-            judge = "great";
-            _game.score.actual += 100000 / _note.timing.length;
-          } else if ((-_threshold.good < (_ref1 = note.timing[this.number] - this.clearTime) && _ref1 < _threshold.good)) {
-            judge = "good";
-            _game.score.actual += 70000 / _note.timing.length;
-          } else {
-            judge = "bad";
-          }
-          judgeLabel = new Label(judge);
-          judgeLabel.x = 450;
-          judgeLabel.y = 450;
-          _game.rootScene.addChild(judgeLabel);
-          judgeLabel.tl.setTimeBased();
-          judgeLabel.tl.fadeOut(300).and().moveY(400, 300).then(function() {
-            return _game.rootScene.removeChild(judgeLabel);
-          });
-          return this.hasClearAnimationStarted = true;
-        }
-      });
-    };
-
-    _poolSprite = function(pool, sprite) {
-      pool.push(sprite);
-      sprite.active = false;
-      if (sprite.addEventListener) {
-        return sprite.addEventListener("removed", function() {
-          return this.active = false;
-        });
-      }
-    };
-
-    _getSprite = function(pool) {
-      var i, value, _i, _len;
-      for (i = _i = 0, _len = pool.length; _i < _len; i = ++_i) {
-        value = pool[i];
-        if (!value.active) {
-          value.active = true;
-          return value;
-        }
-      }
-      console.log("error sprite pool empty");
-      return false;
-    };
-
     document.addEventListener("keydown", function(e) {
       var code, music, value, _i, _len, _ref, _ref1;
-      music = _music;
+      music = _game.music;
       if (_status === "stop") {
         if (e.keyCode === 13) {
           music.play();
@@ -264,9 +141,11 @@
   })();
 
   Note = (function() {
-    var _fallDist, _game, _gen, _note, _noteHeight, _noteWidth, _preAllocate, _renderDist;
+    var _fallDist, _game, _gen, _note, _noteHeight, _noteWidth, _pool, _preAllocate, _renderDist;
 
     _game = null;
+
+    _pool = [];
 
     _fallDist = 550;
 
@@ -282,11 +161,10 @@
       speed: 0
     };
 
-    function Note(game, parms) {
-      var _pool;
+    function Note(game, params) {
       _game = game;
-      _pool = [];
       _note.timing = params.timing;
+      console.log(_note.timing);
       _note.key = params.key;
       _note.speed = params.speed;
     }
@@ -295,14 +173,14 @@
       _note.group = new Group();
       _note.index = 0;
       _renderDist();
-      _preAllocateNote();
+      _preAllocate();
       return _game.rootScene.addChild(_note.group);
     };
 
-    Note.prototype.create = function(currentTime) {
+    Note.prototype.create = function() {
       if (_note.timing[_note.index] != null) {
-        if (currentTime > (_note.timing[_note.index] - (_fallDist / _note.speed))) {
-          return _gen(note.index++);
+        if (_game.music.currentTime > (_note.timing[_note.index] - (_fallDist / _note.speed))) {
+          return _gen(_note.index++);
         }
       }
     };
@@ -325,7 +203,7 @@
       for (_i = 0, _len = _ref.length; _i < _len; _i++) {
         v = _ref[_i];
         note = new Sprite(_noteWidth, _noteHeight);
-        note.image = GameSys.getResouce("img/chara1.png");
+        note.image = _game.assets["img/chara1.png"];
         GameSys.poolSprite(_pool, note);
       }
     };
@@ -350,7 +228,7 @@
       _note.group.addChild(note);
       return note.addEventListener("enterframe", function() {
         var judge, judgeLabel, music, _ref, _ref1;
-        music = _music;
+        music = _game.music;
         note = _note;
         if (this.oldtime != null) {
           this.rotate((music.currentTime - this.oldtime) * 500);
@@ -416,10 +294,6 @@
           return this.active = false;
         });
       }
-    };
-
-    GameSys.getResouce = function(src) {
-      return _game.assets[src];
     };
 
     return GameSys;
